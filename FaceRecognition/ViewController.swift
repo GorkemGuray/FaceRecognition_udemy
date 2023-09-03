49287b59c642f8240d8310b0a6d89e1a594d34d2
Görkem Güray <grkmgry@gmail.com>
2023-09-03 22:49:15 +0300

    final hali

--- FaceRecognition/ViewController.swift
+++ FaceRecognition/ViewController.swift
@@ -6,14 +6,51 @@
 //
 
 import UIKit
+//burası import edilmelidir.
+import LocalAuthentication
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var myLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func signInClicked(_ sender: UIButton) {
+        
+        //biometric doğrulama için LocalAuthentication contextinie ihtiyacımız var.
+        let authContext = LAContext()
+        
+        //ayrıca NSErrorPointer -'a da ihtiyacımız var
+        //aşağıda başında & işreti ile kullanıldığına dikkat edelim.
+        var error: NSError?
+        
+        if authContext.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) {
+            
+            authContext.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: "Is it you") { succes, error in
+                if succes == true {
+                    
+                    //perform segue işlemi main thread'de olması lazım o yüzden, dispatch kullanıyoruz.
+                    DispatchQueue.main.async {
+                        self.performSegue(withIdentifier: "toSecondVC", sender: nil)
+                    }
+                    
+                    
+                } else {
+                    
+                    DispatchQueue.main.async {
+                        self.myLabel.text = "Error!"
+                    }
+                    
+                    
+                    
+                }
+            }
+            
+        }
+        
+    }
 }
 
